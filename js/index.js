06ea992f41af367b10ab7bd3e4dab3e5b19bf4e1
Jwala Singh Baheliya <jaybaheliya@gmail.com>
2020-10-31 23:41:38 +0530

    JB-UIUX-Portfolio
    Hi! My name is Jwala Singh Baheliya also know as Jay (Nickname). I am a Web Developer (UX-UI), and I'm very passionate and dedicated to my work. With 4+ years experience as a professional Web developer, I have acquired the skills and knowledge necessary to make your project a success. I enjoy every step of the design process, from discussion and collaboration to concept and execution, but I find the most satisfaction in seeing the finished product do everything for you that it was created to do.
    
    I have couple of hobbies like to play games on PC , exploring websites , listing calm musics etc. I am fond of PC Games.

---
+++ js/index.js
@@ -0,0 +1,33 @@
+﻿$(document).ready(function () {
+     $('.view-minicv').click(function () {
+         $('.mini-cv').slideDown(500);
+         $('.view-minicv').hide();
+         $('.hide-minicv').show();
+    });
+    $('.hide-minicv').click(function () {
+        $('.mini-cv').slideUp(500);       
+        $('.view-minicv').show();
+        $('.hide-minicv').hide();
+       
+    });
+});
+
+$(window).load(function () {
+    $('.hide-minicv').hide();
+});
+
+//$(document).ready(function () {
+//    var boxWidth = $(".aboutUs").width();
+//    $(".view-minicv").click(function () {
+//        $(".aboutUs").animate({
+//            width: 0
+//        }, function () {
+//            $(".aboutUs").hide();
+//            });
+//    });
+//    $(".slide-right").click(function () {
+//        $(".box").animate({
+//            width: boxWidth
+//        });
+//    });
+//});
